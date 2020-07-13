7129d9298fb631ea143fb9b84a85da7b46931acb
AdamJHowell <adam.j.howell@gmail.com>
2020-07-13 11:59:04 -0600

    2

--- CeilingFan.ino
+++ CeilingFan.ino
@@ -1,15 +1,23 @@
-/* Ceiling Fan, by Adam Howell
- - Moves 3 servos through a range of positions
- - Will eventually control 3 collective servos and 1 speed controller
- - SEE the comments after "//" on each line below
- - CONNECTIONS:
- - Servo connector plugged on pins 3, 6, and 9.
- - If separate wires:
-  - Servo Black or Brown to Gnd.
-  - Servo Red or Orange (Center wire) to +5V
-  - Servo White or Yellow to Signal (Pin 9)
- - V1.02 02/11/13
- Questions: terry@yourduino.com */
+/*
+   Ceiling Fan, by Adam Howell
+   Moves 3 servos through a range of positions
+   Will eventually control 3 collective servos and 1 speed controller
+   SEE the comments after "//" on each line below
+   HARDWARE:
+    Arduino Uno
+    Servo Shield (SainSmart Sensor Shield v4.0)
+    3 servos
+    1 ESC (Electronic Speed Controller)
+   CONNECTIONS:
+      Cyclic/Collective Pitch Mixing (CCPM) Servos plugged into digital pins 3, 6, and 9 on the servo shield.
+      ESC plugged into digital pin 11 on the servo shield.
+    If separate wires:
+      Servo Black or Brown to Gnd.
+      Servo Red or Orange (Center wire) to +5V
+      Servo White or Yellow to Signal (Pin 9)
+   V1.02 02/11/13
+   Based on the "Servo Sweep" example by terry@yourduino.com at http://arduino-info.wikispaces.com/YourDuinoStarter_ServoSweep
+*/
 
 
 /*-----( Import needed libraries )-----*/
@@ -18,9 +26,9 @@
 
 /*-----( Declare Constants and Pin Numbers )-----*/
 // I no longer use the next definition, since I've modified this to use 3 servos.  I'm keeping it to remind me which pins are usable.
-//#define ServoPIN  9  // Can be changed 3,5,6,9,10,11
+//#define ServoPIN  9  // Can be changed 3,5,6,9,10,11  (NOW can be any pin including A0..A5)
 // The minimum and maximum values may need to be adjusted for each servo.  The theoretical range is from 0 to 180.
-#define ServoMIN  0     // This will be the lower value for all servos, 0 is the absolute floor.
+#define ServoMIN  0     // This will be the lower value for all servos, 0 is the absolute floor.  You should avoid using the absolute minimum.
 #define ServoMAX  180   // This will be the higher value for all servos, 180 is the absolute ceiling.
 #define Servo3MIN 70    // This will be the minimum value servo 3 will move to.  This is how high the arm moves up.  Port servo.
 #define Servo3MAX 160   // This will be the maximum value servo 3 will move to.  This is how low the arm moves down.  Port servo.
@@ -31,8 +39,8 @@
 
 
 /*-----( Declare objects )-----*/
-// create servo objects to control servos
-// a maximum of eight servo objects can be created
+// Create servo objects to control servos
+// A maximum of eight servo objects can be created
 Servo MyServo3;    // Servo object for pin 3.
 Servo MyServo6;    // Servo object for pin 6.
 Servo MyServo9;    // Servo object for pin 9.
@@ -43,52 +51,51 @@ int invpos = 0;    // This is an inverted position to accomodate servo reversing
 
 void setup()   /****** SETUP: RUNS ONCE ******/
 {
-//  myservo.attach(ServoPIN);  // attaches the servo on pin 9 to the servo object.
-  MyServo3.attach(3);  // Attach MyServo3 to pin 3.
-  MyServo6.attach(6);  // Attach MyServo6 to pin 6.
-  MyServo9.attach(9);  // Attach MyServo9 to pin 9.
+	//  myservo.attach(ServoPIN);  // attaches the servo on pin 9 to the servo object.
+	MyServo3.attach(3);  // Attach MyServo3 to pin 3.
+	MyServo6.attach(6);  // Attach MyServo6 to pin 6.
+	MyServo9.attach(9);  // Attach MyServo9 to pin 9.
 
 }//--(end setup )---
 
 
 void loop()   /****** LOOP: RUNS CONSTANTLY ******/
 {
-  for( pos = ServoMIN; pos < ServoMAX; pos += 1 )    // Count from our minimum value to our maximum value in steps of 1 degree.
-  {
-    if( pos > Servo3MIN && pos < Servo3MAX )         // This is an upper cap for servo 3.
-    {
-        MyServo3.write(pos);                         // Move the servo to the position in variable 'pos'.
-    }
-    invpos = ServoMAX - pos;                         // Calculate the inveted position by taking the maximum value and subtracting the current postitional value.
-    if( invpos > Servo6MIN && invpos < Servo6MAX )  // If the left servo goes lower than about 35, it hits the main drive gear, so this prevents that interaction.
-    {
-        MyServo6.write(invpos);                      // Move the servo to the position in variable 'invpos'.
-    }
-    if( pos > Servo9MIN && pos < Servo9MAX)          // This is an upper cap for servo 9.
-    {
-        MyServo9.write(pos);                         // Move the servo to the position in variable 'pos'.
-    }
-    delay(10);                                       // Wait 15ms for the servo to reach the position.
-  }
-  delay(1000);
-  for( pos = ServoMAX; pos > ServoMIN; pos-=1 )      // Count from our maximum value to our minimum value in steps of 1 degree.
-  {                                
-    if( pos > Servo3MIN && pos < Servo3MAX )         // This is an upper cap for servo 3.
-    {
-        MyServo3.write(pos);                         // Move the servo to the position in variable 'pos'.
-    }
-    invpos = ServoMAX - pos;                         // Calculate the inveted position by taking the maximum value and subtracting the current postitional value.
-    if( invpos > Servo6MIN && invpos < Servo6MAX )  // If the left servo goes lower than about 35, it hits the main drive gear, so this prevents that interaction.
-    {
-        MyServo6.write(invpos);                      // Move the servo to the position in variable 'invpos'.
-    }
-    if( pos > Servo9MIN && pos < Servo9MAX)          // This is an upper cap for servo 9.
-    {
-        MyServo9.write(pos);                         // Move the servo to the position in variable 'pos'.
-    }
-    delay(10);                                       // Wait 15ms for the servo to reach the position.
-  }
-  delay(1000);
+	for ( pos = ServoMIN; pos < ServoMAX; pos += 1 )   // Count from our minimum value to our maximum value in steps of 1 degree.
+	{
+		if ( pos > Servo3MIN && pos < Servo3MAX )        // This is an upper cap for servo 3.
+		{
+			MyServo3.write(pos);                         // Move the servo to the position in variable 'pos'.
+		}
+		invpos = ServoMAX - pos;                         // Calculate the inveted position by taking the maximum value and subtracting the current postitional value.
+		if ( invpos > Servo6MIN && invpos < Servo6MAX ) // If the left servo goes lower than about 35, it hits the main drive gear, so this prevents that interaction.
+		{
+			MyServo6.write(invpos);                      // Move the servo to the position in variable 'invpos'.
+		}
+		if ( pos > Servo9MIN && pos < Servo9MAX)         // This is an upper cap for servo 9.
+		{
+			MyServo9.write(pos);                         // Move the servo to the position in variable 'pos'.
+		}
+		delay(10);                                       // Wait 10ms for the servo to reach the position.
+	}
+	delay(1000);
+	for ( pos = ServoMAX; pos > ServoMIN; pos -= 1 )   // Count from our maximum value to our minimum value in steps of 1 degree.
+	{
+		if ( pos > Servo3MIN && pos < Servo3MAX )        // This is an upper cap for servo 3.
+		{
+			MyServo3.write(pos);                         // Move the servo to the position in variable 'pos'.
+		}
+		invpos = ServoMAX - pos;                         // Calculate the inveted position by taking the maximum value and subtracting the current postitional value.
+		if ( invpos > Servo6MIN && invpos < Servo6MAX ) // If the left servo goes lower than about 35, it hits the main drive gear, so this prevents that interaction.
+		{
+			MyServo6.write(invpos);                      // Move the servo to the position in variable 'invpos'.
+		}
+		if ( pos > Servo9MIN && pos < Servo9MAX)         // This is an upper cap for servo 9.
+		{
+			MyServo9.write(pos);                         // Move the servo to the position in variable 'pos'.
+		}
+		delay(10);                                       // Wait 10ms for the servo to reach the position.
+	}
+	delay(1000);
 
 }//--(end main loop )---
-
