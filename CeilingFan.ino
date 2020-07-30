cd980fa4e306aa8bea737edb1edda95ed523a6e5
AdamJHowell <adam.j.howell@gmail.com>
2020-07-30 12:08:33 -0600

    16 - Formatting changes

--- CeilingFan.ino
+++ CeilingFan.ino
@@ -1,32 +1,32 @@
 /*
-    Ceiling Fan
-    A ceiling fan made from a R/C helicopter.
-    Create 2020-07-28
-    by Adam Howell
-    https://github.com/AdamJHowell
+	Ceiling Fan
+	A ceiling fan made from a R/C helicopter.
+	Create 2020-07-28
+	by Adam Howell
+	https://github.com/AdamJHowell
 
-    Hardware:
-      HiLetgo ESP8266 NodeMCU ESP-12E
-      Hobby King HK450 CCPM radio controlled helicopter
-      Turnigy Typhoon 450H 2215H Heli Motor 3550kv
-      Hobbyking SS Series 25-30A ESC
-      4x Corona 919MG Digital Metal Gear Servo
-      Antec NEO480 PSU
-      Electronics-Salon 24/20-pin ATX DC Power Supply Breakout Board Module
-      Radio Shack Experimentor 350 breadboard
-      Hookup wire (various)
-   CONNECTIONS:
-      Cyclic/Collective Pitch Mixing (CCPM) Servos plugged into digital pins D2, D3, and D4.
-      ESC plugged into digital pin D0.
-      Twelve servo objects can be created on most boards using GPIO pins.
-    If separate wires:
-      Servo Black or Brown to Gnd.
-      Servo Red or Orange (Center wire) to +5V
-      Servo White or Yellow to Signal (Pin 9)
-    API:
-      To simplify the API, only one digit will be used for servo ranges.
-      This means a range of 0 to 9.
-      I may switch to a 0 to 99 range in a later release.
+	Hardware:
+		HiLetgo ESP8266 NodeMCU ESP-12E
+		Hobby King HK450 CCPM radio controlled helicopter
+		Turnigy Typhoon 450H 2215H Heli Motor 3550kv
+		Hobbyking SS Series 25-30A ESC
+		4x Corona 919MG Digital Metal Gear Servo
+		Antec NEO480 PSU
+		Electronics-Salon 24/20-pin ATX DC Power Supply Breakout Board Module
+		Radio Shack Experimentor 350 breadboard
+		Hookup wire (various)
+	CONNECTIONS:
+		Cyclic/Collective Pitch Mixing (CCPM) Servos plugged into digital pins D2, D3, and D4.
+		ESC plugged into digital pin D0.
+		Twelve servo objects can be created on most boards using GPIO pins.
+	If separate wires:
+		Servo Black or Brown to Gnd.
+		Servo Red or Orange (Center wire) to +5V
+		Servo White or Yellow to Signal (Pin 9)
+	API:
+		To simplify the API, only one digit will be used for servo ranges.
+		This means a range of 0 to 9.
+		I may switch to a 0 to 99 range in a later release.
 */
 
 #include <Servo.h>
@@ -34,17 +34,17 @@
 #include <PubSubClient.h>     // PubSub is the MQTT API.
 
 // WiFi and MQTT constants.
-const char* ssid = "Red";
-const char* password = "8012254722";
-const char* mqttServer = "192.168.55.200";
+const char * ssid = "Red";
+const char * password = "8012254722";
+const char * mqttServer = "192.168.55.200";
 const int mqttPort = 2112;
-const char* mqttTopic = "mqttServo";
+const char * mqttTopic = "mqttServo";
 const int throttlePin = 16;           // Use GPIO16 (D0) for the throttle (ESC).
 const int rudderPin = 5;              // Use GPIO5 (D1) for the rudder.
 const int collective1Pin = 4;         // Use GPIO4 (D2) for the collective1.
 const int collective2Pin = 0;         // Use GPIO0 (D3) for the collective2.
 const int collective3Pin = 2;         // Use GPIO2 (D4) for the collective3.
-int throttlePos = 0;              
+int throttlePos = 0;
 int rudderPos = 90;
 int collective1Pos = 90;
 int collective2Pos = 90;
@@ -60,17 +60,17 @@ Servo collective3Servo; // Create servo object to control one of the three colle
 
 
 /**
- * moveServo() reads the current position, and gradually moves the servo to the new position.
- */
+   moveServo() reads the current position, and gradually moves the servo to the new position.
+*/
 int moveServo( Servo thisServo, int curPos, int newPos )
 {
-  if ( newPos < 0 )               // Check for an invalid value.
+  if ( newPos < 0 )              // Check for an invalid value.
   {
     Serial.print( "moveServo() was given an invalid servo position: " );
     Serial.println( newPos );
     newPos = 5;                   // Set the servo to a low, but valid, position.
   }
-  else if ( newPos > 180 )        // Check for an invalid value.
+  else if ( newPos > 180 )       // Check for an invalid value.
   {
     Serial.print( "moveServo() was given an invalid servo position: " );
     Serial.println( newPos );
@@ -169,7 +169,7 @@ void fatoLightChange( int receivedValue )
    callback() handles MQTT subscriptions.
    When a message comes in on a topic we have subscribed to, this function is executed.
 */
-void callback( char* topic, byte* payload, unsigned int length )
+void callback( char * topic, byte * payload, unsigned int length )
 {
   Serial.println();
   Serial.print( "Message arrived [" );
@@ -177,59 +177,59 @@ void callback( char* topic, byte* payload, unsigned int length )
   Serial.print( "] " );
   for ( int i = 0; i < length; i++ )
   {
-    char receivedKey = ( char )payload[i];
+    char receivedKey = ( char ) payload[i];
     Serial.print( receivedKey );
 
-    if ( receivedKey == 't' )        // Process throttle changes.
+    if ( receivedKey == 't' )       // Process throttle changes.
     {
       if ( length > 1 )
       {
-        char receivedValue = ( char )payload[i + 1];    // Store the 0-9 value in receivedValue.
+        char receivedValue = ( char ) payload[i + 1];    // Store the 0-9 value in receivedValue.
         i++;                                            // Increment the index since it was consumed.
         throttleChange( receivedValue - '0' );          // Send the 0-9 value.
       }
     }
-    else if ( receivedKey == 'c' )        // Process collective changes.
+    else if ( receivedKey == 'c' )       // Process collective changes.
     {
       if ( length > 1 )
       {
-        char receivedValue = ( char )payload[i + 1];    // Store the 1-9 value in receivedValue.
+        char receivedValue = ( char ) payload[i + 1];    // Store the 1-9 value in receivedValue.
         i++;                                            // Increment the index since it was consumed.
         collectiveChange( receivedValue - '0' );        // Send the 1-9 value.
       }
     }
-    else if ( receivedKey == 'r' )        // Process rudder changes.
+    else if ( receivedKey == 'r' )       // Process rudder changes.
     {
       if ( length > 1 )
       {
-        char receivedValue = ( char )payload[i + 1];    // Store the 1-9 value in receivedValue.
+        char receivedValue = ( char ) payload[i + 1];    // Store the 1-9 value in receivedValue.
         i++;                                            // Increment the index since it was consumed.
         rudderChange( receivedValue - '0' );            // Send the 1-9 value.
       }
     }
-    else if ( receivedKey == 'f' )        // Process floodlight changes.
+    else if ( receivedKey == 'f' )       // Process floodlight changes.
     {
       if ( length > 1 )
       {
-        char receivedValue = ( char )payload[i + 1];    // Store the 0-1 value in receivedValue.
+        char receivedValue = ( char ) payload[i + 1];    // Store the 0-1 value in receivedValue.
         i++;                                            // Increment the index since it was consumed.
         floodLightChange( receivedValue - '0' );        // Send the 0-1 value.
       }
     }
-    else if ( receivedKey == 'l' )        // Process green TLOF circle LED changes.
+    else if ( receivedKey == 'l' )       // Process green TLOF circle LED changes.
     {
       if ( length > 1 )
       {
-        char receivedValue = ( char )payload[i + 1];    // Store the 0-1 value in receivedValue.
+        char receivedValue = ( char ) payload[i + 1];    // Store the 0-1 value in receivedValue.
         i++;                                            // Increment the index since it was consumed.
         tlofLightChange( receivedValue - '0' );         // Send the 0-1 value.
       }
     }
-    else if ( receivedKey == 'a' )        // Process white FATO square LED changes.
+    else if ( receivedKey == 'a' )       // Process white FATO square LED changes.
     {
       if ( length > 1 )
       {
-        char receivedValue = ( char )payload[i + 1];    // Store the 0-1 value in receivedValue.
+        char receivedValue = ( char ) payload[i + 1];    // Store the 0-1 value in receivedValue.
         i++;                                            // Increment the index since it was consumed.
         fatoLightChange( receivedValue - '0' );         // Send the 0-1 value.
       }
@@ -244,10 +244,10 @@ void callback( char* topic, byte* payload, unsigned int length )
 void reconnect()
 {
   // Loop until we're reconnected.
-  while ( !mqttClient.connected() )
+  while ( !mqttClient.connected())
   {
     Serial.print( "Attempting MQTT connection..." );
-    if ( mqttClient.connect( "ESP8266 Client" ) )         // Attempt to connect using the designated clientID.
+    if ( mqttClient.connect( "ESP8266 Client" ))        // Attempt to connect using the designated clientID.
     {
       Serial.println( "connected" );
       mqttClient.subscribe( mqttTopic );                // Subscribe to the designated MQTT topic.
@@ -255,7 +255,7 @@ void reconnect()
     else
     {
       Serial.print( " failed, return code: " );
-      Serial.print( mqttClient.state() );
+      Serial.print( mqttClient.state());
       Serial.println( " try again in 5 seconds" );
       // Wait 5 seconds before retrying.
       delay( 5000 );
@@ -294,19 +294,19 @@ void setup()
 
   int i = 0;
   /*
-     WiFi.status() return values:
-     0 : WL_IDLE_STATUS when WiFi is in process of changing between statuses
-     1 : WL_NO_SSID_AVAIL in case configured SSID cannot be reached
-     3 : WL_CONNECTED after successful connection is established
-     4 : WL_CONNECT_FAILED if password is incorrect
-     6 : WL_DISCONNECTED if module is not configured in station mode
+  	WiFi.status() return values:
+  	0 : WL_IDLE_STATUS when WiFi is in process of changing between statuses
+  	1 : WL_NO_SSID_AVAIL in case configured SSID cannot be reached
+  	3 : WL_CONNECTED after successful connection is established
+  	4 : WL_CONNECT_FAILED if password is incorrect
+  	6 : WL_DISCONNECTED if module is not configured in station mode
   */
-  while ( WiFi.status() != WL_CONNECTED )     // Wait for the WiFi to connect.
+  while ( WiFi.status() != WL_CONNECTED )    // Wait for the WiFi to connect.
   {
     delay( 1000 );
     Serial.println( "Waiting for a connection..." );
     Serial.print( "WiFi status: " );
-    Serial.println( WiFi.status() );
+    Serial.println( WiFi.status());
     Serial.print( ++i );
     Serial.println( " seconds" );
   }
@@ -314,7 +314,7 @@ void setup()
   Serial.println( '\n' );
   Serial.println( "Connection established!" );
   Serial.print( "IP address:\t" );
-  Serial.println( WiFi.localIP() );           // Send the IP address of the ESP8266 to the computer.
+  Serial.println( WiFi.localIP());           // Send the IP address of the ESP8266 to the computer.
 } // End of setup() function.
 
 
@@ -326,7 +326,7 @@ void loop()
   int pos;    // A position variable for the servo.
   // My servos are fully counter-clockwise when 0 is written to them.
 
-  if ( !mqttClient.connected() )
+  if ( !mqttClient.connected())
   {
     reconnect();
   }
--- README.md
+++ README.md
@@ -6,42 +6,45 @@ My intent is to use this as a ceiling fan, with a 450-class electric helicopter 
 
 This would also work well as a bench test for any radio-controlled project.
 
-https://github.com/AdamJHowell
+<https://github.com/AdamJHowell>
 
-###Setup
+## Setup
 
 **Hardware:**
- - HiLetgo ESP8266 NodeMCU ESP-12E
- - Hobby King HK450 CCPM radio-controlled helicopter
- - Turnigy Typhoon 450H 2215H Heli Motor 3550kv
- - Hobbyking SS Series 25-30A ESC
- - 4x Corona 919MG Digital Metal Gear Servo
- - Antec NEO480 PSU
- - Electronics-Salon 24/20-pin ATX DC Power Supply Breakout Board Module
- - Radio Shack Experimentor 350 breadboard
- - Hookup wire (various)
+
+- HiLetgo ESP8266 NodeMCU ESP-12E
+- Hobby King HK450 CCPM radio-controlled helicopter
+- Turnigy Typhoon 450H 2215H Heli Motor 3550kv
+- Hobbyking SS Series 25-30A ESC
+- 4x Corona 919MG Digital Metal Gear Servo
+- Antec NEO480 PSU
+- Electronics-Salon 24/20-pin ATX DC Power Supply Breakout Board Module
+- Radio Shack Experimentor 350 breadboard
+- Hookup wire (various)
 
 **CONNECTIONS:**
- - Cyclic/Collective Pitch Mixing (CCPM) Servos plugged into digital pins D2, D3, and D4
- - ESC plugged into digital pin D0
- - Rudder servo plugged into digital pin D1
- - Connect black or brown servo wires to ground
- - Connect red servo wires to +5V
- - Connect white, orange, or yellow servo wires to signal
- - Ensure the ESC is provided enough amperage
 
-###API
+- Cyclic/Collective Pitch Mixing (CCPM) Servos plugged into digital pins D2, D3, and D4
+- ESC plugged into digital pin D0
+- Rudder servo plugged into digital pin D1
+- Connect black or brown servo wires to ground
+- Connect red servo wires to +5V
+- Connect white, orange, or yellow servo wires to signal
+- Ensure the ESC is provided enough amperage
+
+## API
 
 The API for this project is a simple two-character string, where the first character is a key designating the device to be controlled, and the second character (a digit) is a value representing the setting for that device.
 For lighting, zero extinguishes the light, and any non-zero value will illuminate the light.
 
 **First character:**
- - t = throttle servo
- - c = collective servo
- - r = rudder servo
- - f = floodlight LEDs
- - l = green TLOF circle LEDs
- - a = white FATO square LEDs
+
+- t = throttle servo
+- c = collective servo
+- r = rudder servo
+- f = floodlight LEDs
+- l = green TLOF circle LEDs
+- a = white FATO square LEDs
 
 **Second character:**
 
